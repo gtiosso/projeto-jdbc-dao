68ceffb9f43a2a9fede81ad9529b96c2b2755fc0
Guilherme Tiosso <guilherme.tiosso@gmail.com>
2019-04-07 01:56:17 -0300

    Seller Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,21 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
-		Department obj = new Department(1, "Books");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Department department = new Department(1, "Books");
+		
+		Seller obj = new Seller(1, "Guilherme", "guilherme.tiosso", sdf.parse("26/06/1992"), 2000.0, department);
+		
 		
 		System.out.println(obj);
 	}
